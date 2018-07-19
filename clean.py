edea6f922a129efee6eb23fdf49057cc0c21c71f
Yukino.T <zeromaru_43729@hotmail.com>
2018-07-19 15:36:10 +0000

    add more cleaning pattern

--- clean.py
+++ clean.py
@@ -5,8 +5,36 @@ import pandas as pd
 
 def cleanLine(line):
 	user_pattern = re.compile(r'@(\w){1,15}\s')
+	emoji_pattern = re.compile("["
+		u"\U0001F600-\U0001F64F"  # emoticons
+		u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+		u"\U0001F680-\U0001F6FF"  # transport & map symbols
+		u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+						   "]+", flags=re.UNICODE)
+	url_pattern = re.compile(r'(https:\S+)')
+	hashtag_pattern = re.compile(r'(\#\S+)')
+
+	duplicate_space = re.compile(r'(\s{2,})')
+
+	non_char = re.compile(r'"|\'|\\|/|!|_|-|—|=|\+|\.|\n|\(|\)|\*|•|@|\?|\^|~|“|”|\[|\]|{|}|:|\|')
+	special_char = re.compile(r'&\S+;')
+
+	# pattern create and remove space
 	line = re.sub(user_pattern, ' ', line)
+
+	# clear all unneccessary text
+	line = re.sub(emoji_pattern, '', line)
+	line = re.sub(url_pattern, '', line)
+	line = re.sub(hashtag_pattern, '', line)
+	line = re.sub(non_char, '', line)
+	line = re.sub(special_char, '', line)
+
+	line = re.sub(duplicate_space, ' ', line)
+
 	return line.replace('\n',' ')
+
+def stripping(line):
+	return line.lstrip(' ').rstrip(' ')
 
 try:
 	file = open(sys.argv[1],'r')
@@ -27,11 +55,11 @@ for line in file.readlines():
 	if bool(pattern.search(line)):
 		tableData = tableData.append({
 			'time':line[1:-3],
-			'text':tmp.replace('\ufeff','')
+			'text':stripping(tmp.replace('\ufeff',''))
 		}, ignore_index=True)
 		tmp = ''
-		if '"' in tmp:
-			print('found quote at ',i)
+		# if '"' in tmp:
+		# 	print('found quote at ',i)
 		# print(i)
 		i+=1
 	else:
