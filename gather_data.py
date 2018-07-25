da73061ab40291f9cc0112abb1b07dae7a97cb66
Yukino.T <zeromaru_43729@hotmail.com>
2018-07-25 14:54:05 +0000

    make gather file read entity and sum to each file in filtered

--- gather_data.py
+++ gather_data.py
@@ -1,10 +1,40 @@
 import glob
 
-entity = 'dtac'
-fileList = glob.glob('data/filtered/{}*.txt'.format(entity))
-print(fileList)
+try:
+	entityFile = open('entity.txt','r')
+except IndexError:
+	print('Please give the input file')
+	exit(0)
+except FileNotFoundError:
+	print('File not found')
+	exit(0)
 
-for filePath in fileList:
-	file = open(filePath, 'r')
-	print(file.readlines())
-	
+try:
+	readed = {}
+	entities = {}
+	for line in entityFile.readlines():
+		if line[-1] == '\n':
+			line = line[:-1]
+		tmp = line.split(',')
+		entities[tmp[0]] = {}
+		entities[tmp[0]]['filter'] = tmp
+		entities[tmp[0]]['txt'] = []
+
+	for entity in entities.keys():
+		readed[entity] = []
+		fileList = glob.glob('data/filtered/{}*.txt'.format(entity))
+		for filePath in fileList:
+			file = open(filePath, 'r')
+			readed[entity] += file.readlines()
+
+except FileNotFoundError:
+	print('File not found')
+	exit(0)
+
+# print(readed)
+for key, content in readed.items():
+	print(key)
+	file = open('data/filtered/{}.txt'.format(key),'w')
+	for line in content:
+		file.write(line)
+	file.close()
