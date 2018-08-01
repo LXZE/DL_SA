249d34e74d8c0f9bd277e5252f2da9291b645308
Yukino T <zeromaru_43729@hotmail.com>
2018-08-02 02:30:23 +0700

    modified pipeline, check dir before run

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,11 @@
 import glob
 from subprocess import call
+import pathlib
 
 # clean
 rawFileList = glob.glob('data/twit_*')
 print(rawFileList)
+pathlib.Path('data/clean').mkdir(parents=True, exist_ok=True) 
 for file in rawFileList:
 	code = call(['python', 'clean.py', file])
 	print(code)
@@ -11,6 +13,7 @@ for file in rawFileList:
 # filter
 cleanFileList = glob.glob('data/clean/*')
 print(cleanFileList)
+pathlib.Path('data/filtered').mkdir(parents=True, exist_ok=True) 
 for file in cleanFileList:
 	code = call(['python', 'filter.py', file])
 
