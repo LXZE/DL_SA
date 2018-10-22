50a4476a85bdf1940e66343556c6d8b8105e900f
Yukino T <zeromaru_43729@hotmail.com>
2018-10-22 21:02:14 +0700

    add word error checker and word error table lookup

--- utility/word_error_check.py
+++ utility/word_error_check.py
@@ -1,6 +1,7 @@
 import re, sys
 import pandas as pd
 import pythainlp as pyt
+import clean
 try:
 	# NOTE: input must be cleaned
 	dataset = pd.read_csv(sys.argv[1], escapechar="\\")
@@ -13,17 +14,41 @@ except FileNotFoundError:
 
 prop = [0, 0]
 thai_pattern = re.compile(r'([\u0E00-\u0E7F฿%]+)')
-rep = ['งง', 'สส', 'นน', 'รร', 'ออ']
+# repeat_pattern = re.compile(r'([^\d\s]+?)\1+')
+char_repeat_pattern = re.compile(r'([^\d\s]{1})\1+')
+rep = ['งง', 'สส', 'นน', 'รร', 'ออ', 'บบ']
+
+pattern_count = {}
+
 for idx, row in dataset.iterrows():
-	sentence = row['text']
+	sentence = clean.fixing(row['text'])
 	result = pyt.word_tokenize(sentence, engine='newmm')
+
+	crit0 = [bool(char_repeat_pattern.search(token)) and bool(thai_pattern.search(token)) for token in result]
+	res0 = [(result[idx-1: idx+1]) for idx, log in enumerate(crit0) if log]
+
 	crit2 = [len(token) == 2 and bool(thai_pattern.search(token)) and token[0] == token[1] and (token not in rep) for idx, token in enumerate(result)]
 	res2 = [(result[idx-1: idx+1]) for idx, log in enumerate(crit2) if log]
-	if any([len(token) == 1 and bool(thai_pattern.search(token)) for token in result]):
-		prop[0] += 1
-	elif any(crit2):
-		prop[0] += 1
-		print(res2)
-	else:
-		prop[1] += 1
-print(prop)
+
+	if any(crit0):
+		# print(res0)
+		for patt in res0:
+			if len(patt) == 0 or patt[0] == ' ':
+				continue
+			patt = '+'.join(patt)
+			if patt in pattern_count:
+				pattern_count[patt] += 1 
+			else:
+				pattern_count[patt] = 1
+
+	# if any([len(token) == 1 and bool(thai_pattern.search(token)) for token in result]):
+	# 	prop[0] += 1
+	# elif any(crit2):
+	# 	prop[0] += 1
+	# 	print(res2)
+	# else:
+	# 	prop[1] += 1
+patt_dict = [(k, pattern_count[k]) for k in sorted(pattern_count, key=pattern_count.get, reverse=True)]
+for k,v in patt_dict:
+	print(k, v)
+# print(prop)
