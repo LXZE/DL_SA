cbb1b3710844b00ca331ea32a1ad11862201e94f
Yukino T <zeromaru_43729@hotmail.com>
2018-10-22 22:13:03 +0700

    add word lookup table as a submodule

--- test_vector.py
+++ test_vector.py
@@ -2,7 +2,7 @@ import glob, re, time, sys
 import pandas as pd
 import numpy as np
 from gensim.models import KeyedVectors
-from gensim.models import FastText as fText
+import fastText as ft
 import clean
 
 vector_model_dir = '../model/thai2vec/word2vec/' 
@@ -60,9 +60,11 @@ def edits2(word):
 	"All edits that are two edits away from `word`."
 	return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
-# model = fText.load_fasttext_format(vector_model_path)
-print(vector_model.get_word_vector('มาก'))
-print(vector_model.get_word_vector('มากก'))
+path = '../model/cc.th.300.bin'
+model = ft.load_model(path)
+# model = fText.load_model(vector_model_dir)
+print(model.get_word_vector('มาก'))
+print(model.get_word_vector('มากก'))
 
 # print(correction('เอไอเอส'))
 # print(correction('นะค่ะ'))
